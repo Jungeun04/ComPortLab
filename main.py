6ba0dda7d9aac5c44086e35778d9c76ab0543146
Me <h>
2024-01-23 17:43:06 +0900

    해상도 감지 추가

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def main():
     root.title("ComPortLab")
     root.minsize(width=500, height=350)  # 예시 크기, 필요에 따라 조절
 
+    # 모니터 해상도 감지
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # 모니터의 중심 좌표
+    x = (screen_width / 2) - (500 / 2)
+    y = (screen_height / 2) - (350 / 2)
+
+    root.geometry(f'+{int(x)}+{int(y)}')
+
     # Notebook 위젯 생성
     notebook = ttk.Notebook(root)
     notebook.pack(expand=True, fill="both")
